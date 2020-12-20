631ea07f3ea63fc69bfbcd42aaf257f62580263d
ArhumJ <arhumjain10@gmail.com>
2020-12-19 21:37:12 -0800

    Get gCost&fCost
    Implemented functionality to associate each node in grid with a gCost & fCost

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import math
-start = (2,2)
-end = (7,2)
 ids = []
 null = []
 closed = []
 open = []
-
 def buildGrid():
     idcount = 1
     for i in range(1, 11):
@@ -14,18 +10,16 @@ def buildGrid():
             null[i - 1].append(Node(idcount, j - 1, i - 1))
             idcount += 1
 class Node():
+    end = (7,2)
+    start = (2,7)
     def __init__(self, id, x, y, parent = None):
         self.id = id
         self.x = x
         self.y = y
         self.parent = parent
-        self.gCost = self.findGCost()
-        self.hCost = self.findHCost()
-        #print(self.hCost)
-    def findGCost(self):
-        pass
-    def findHCost(self):
-        global end
+        self.hCost = self.findHCost(Node.end)
+        self.gCost = self.findHCost(Node.start)
+    def findHCost(self, end):
         endx = end[0]
         endy = end[1]
         if self.y == endy:
@@ -37,36 +31,29 @@ class Node():
             junctiony = endy
             if junctionx > endx:
                 junctionx = endx
-                if self.y > endy:
-                    junctiony = self.y-(max(endx, self.x) - min(endx, self.x))
-                    return
-                    pass
+                if self.y < endy:
+                    junctiony = self.y+(max(endx, self.x) - min(endx, self.x))
+                    return (((max(junctionx, self.x)) - (min(junctionx, self.x))) * 14) - ((junctiony - endy)*10)
                 else:
-
-                pass
+                    junctiony = self.y-(max(endx, self.x) - min(endx, self.x))
+                    return (((max(junctionx, self.x)) - (min(junctionx, self.x))) * 14) + ((junctiony - endy) * 10)
             else:
                 return ((junctionx-self.x)*14)+((endx-junctionx)*10)
-                #calculate hcost
 
         elif self.x > endx:
             junctionx = self.x-(max(endy, self.y) - min(endy, self.y))
             junctiony = endy
             if junctionx < endx:
-                pass
-                #calculate hcost
+                junctionx = endx
+                if self.y < endy:
+                    junctiony = self.y + (max(endx, self.x) - min(endx, self.x))
+                    return (((max(junctionx, self.x)) - (min(junctionx, self.x))) * 14) - ((junctiony - endy) * 10)
+                else:
+                    junctiony = self.y - (max(endx, self.x) - min(endx, self.x))
+                    return (((max(junctionx, self.x)) - (min(junctionx, self.x))) * 14) + ((junctiony - endy) * 10)
             else:
                 return ((self.x-junctionx)*14)+((junctionx - endx)*10)
-
-
-
-        pass
-
-
 buildGrid()
-for i in range(1,10):
-    for j in range(1,10):
-        print(null[i][j].hCost, ":", null[i][j].x, null[i][j].y)
-
-
-
-print("Hello WOrld!")
+for i in range(0,10):
+    for j in range(0,10):
+        print(null[i][j].fCost, ":", null[i][j].x, null[i][j].y)
