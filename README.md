8b1508440279015b5c3eb07eddf0b9853e31cce0
ArhumJ <arhumjain10@gmail.com>
2021-01-05 15:29:34 -0800

    finish

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # AStar Pathfinder Visualizer
- Working on making an A* Pathfinder\
-Need to try and build algorithm first then try and build visualizer
+A* Pathfinding visualizer - 1/5/2021
+
+-> Click on two points to define start and end points
+
+-> Click and drag to draw out obstacles
+
+-> Space to watch the algorithm find the shortest path!
+
+-> Hit "v" before running the program to watch the algorithm in action!
+
+(Might add more features like timer and find shortest path between multiple sets of points...)
--- main.py
+++ main.py
@@ -1,32 +1,35 @@
+import pygame
 ids = []
 grid = []
-#Not known
-null = []
-#Visited
 closed = []
-#Seen but not visited
 open = []
-def buildGrid():
-    idcount = 1
-    for i in range(1, 11):
-        grid.append([])
-        null.append([])
-        for j in range(1, 11):
-            null[i - 1].append(Node(idcount, j - 1, i - 1))
-            grid[i - 1].append(Node(idcount, j - 1, i - 1))
-            idcount += 1
+path = []
 class Node():
-    end = (7,2)
-    start = (2,7)
+    end = (-1,-1)
+    start = (-1,-1)
     def __init__(self, id, x, y, parent = None):
+        global grid
+        global null
         self.isObstacle = False
         self.id = id
         self.x = x
         self.y = y
         self.parent = parent
-        self.hCost = self.findHCost(Node.end)
-        self.gCost = self.findHCost(Node.start)
-        self.fCost = self.hCost+self.gCost
+        self.hCost = None
+        self.gCost = None
+        self.fCost = None
+        grid[self.y].append(self)
+    def addClosed(self):
+        global closed
+        closed.append(self)
+    def addOpen(self):
+        global open
+        open.append(self)
+    def getPath(self):
+        global path
+        if self.parent != None:
+            path.append((self))
+            self.parent.getPath()
     def findHCost(self, end):
         endx = end[0]
         endy = end[1]
@@ -61,20 +64,137 @@ class Node():
                     return (((max(junctionx, self.x)) - (min(junctionx, self.x))) * 14) + ((junctiony - endy) * 10)
             else:
                 return ((self.x-junctionx)*14)+((junctionx - endx)*10)
+def findAdjacent(x, y):
+    for dx in range(-1, 2):
+        for dy in range(-1,2):
+            if dx != 0 or dy != 0:
+                if (x + dx > -1) and (x+dx < int(len(grid[0]))) and (y+dy > -1) and (y+dy < int(len(grid))):
+                    if grid[y+dy][x+dx] not in closed and grid[y+dy][x+dx].isObstacle == False:
+                        grid[y+dy][x+dx].hCost = grid[y+dy][x+dx].findHCost(Node.end)
+                        if (dx == -1 or dx == 1) and (dy == -1 or dy == 1):
+                            gcost = grid[y][x].gCost + 14
+                            fcost = gcost + grid[y+dy][x+dx].hCost
+
+                        else:
+                            gcost = grid[y][x].gCost + 10
+                            fcost = gcost + grid[y+dy][x+dx].hCost
+                        if grid[y+dy][x+dx].parent != None:
+                            if fcost < grid[y+dy][x+dx].fCost:
+                                grid[y+dy][x+dx].parent = grid[y][x]
+                                grid[y+dy][x+dx].gCost = gcost
+                                grid[y+dy][x+dx].fCost = fcost
+                        else:
+                            grid[y+dy][x+dx].parent = grid[y][x]
+                            grid[y+dy][x+dx].gCost = gcost
+                            grid[y+dy][x+dx].fCost = fcost
+                            grid[y+dy][x + dx].addOpen()
+def buildGrid():
+    global grid
+    idcount = 1
+    for i in range(1, 51):
+        grid.append([])
+        for j in range(1, 51):
+            Node(idcount, j - 1, i - 1)
+            idcount += 1
+def windowToBoardCoord(win_x, win_y):
+    board_x = win_x // (WINWIDTH // 50)
+    board_y = win_y // (WINWIDTH // 50)
+    return board_x, board_y
+def drawRect(color, row, column):
+    pygame.draw.rect(win, (color),
+                    (((column * (WINWIDTH) // 50)), ((row * (WINWIDTH) // 50))
+                    , (WINWIDTH - 24) // 50, (WINHEIGHT - 24) // 50))
+def bgupdategrid(visualizer):
+    for row in range(0,50):
+        for column in range(0,50):
+            if visualizer == True:
+                if grid[row][column].isObstacle == True:
+                    drawRect(BLACK, row, column)
+                elif grid[row][column] in path:
+                    drawRect(BLUE, row, column)
+                elif (row == Node.start[1] and column == Node.start[0]) or (row == Node.end[1] and column == Node.end[0]): #(row == Node.start[0] or row == Node.end[0]) and (column == Node.end[1] or column == Node.start[1]):
+                    drawRect(BLUE, row, column)
+                elif grid[row][column] in open:
+                    drawRect(GREEN, row, column)
+                elif grid[row][column] in closed:
+                    drawRect(RED, row, column)
+                else:
+                    drawRect(WHITE, row, column)
+            else:
+                if grid[row][column].isObstacle == True:
+                    drawRect(BLACK, row, column)
+                elif grid[row][column] in path:
+                    drawRect(BLUE, row, column)
+                elif (row == Node.start[1] and column == Node.start[0]) or (row == Node.end[1] and column == Node.end[0]): #(row == Node.start[0] or row == Node.end[0]) and (column == Node.end[1] or column == Node.start[1]):
+                    drawRect(BLUE, row, column)
+                else:
+                    drawRect(WHITE, row, column)
+
 buildGrid()
-for i in range(0,10):
-    for j in range(0,10):
-        print(null[i][j].hCost, ":", null[i][j].x, null[i][j].y)
-
-
-end = null[Node.end[0]][Node.end[1]]
-closed.append(null[Node.start[0]][Node.start[1]])
-null[Node.start[0]].pop(Node.start[1])
-
-
-currentnode = 9
-while end not in closed:
-    pass
-
-
-
+WINWIDTH = 800
+WINHEIGHT = 800
+WHITE = (200,200,200)
+BLACK = (100,100,100)
+GREEN = 124,252,0
+RED = (255,0,0)
+BLUE = (0,0,255)
+startset = False
+endset = False
+doPathFind = False
+drawObstacle = False
+visualizer = False
+pygame.init()
+win = pygame.display.set_mode((WINWIDTH,WINHEIGHT))
+pygame.display.set_caption("A* Pathfinding Visualizer")
+run = True
+currentnode = None
+end = None
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if startset == False and endset == False:
+                mouse_x, mouse_y = event.pos
+                board_x, board_y = windowToBoardCoord(mouse_x, mouse_y)
+                Node.start = (board_x, board_y)
+                currentnode = grid[Node.start[1]][Node.start[0]]
+                closed.append(grid[Node.start[1]][Node.start[0]])
+                grid[Node.start[1]][Node.start[0]].gCost = 0
+                startset = True
+            elif startset == True and endset == False:
+                mouse_x, mouse_y = event.pos
+                board_x, board_y = windowToBoardCoord(mouse_x, mouse_y)
+                Node.end = (board_x, board_y)
+                endset = True
+                end = grid[Node.end[1]][Node.end[0]]
+            elif startset == True and endset == True:
+                drawObstacle = True
+        elif pygame.mouse.get_pressed()[0] and drawObstacle == True:
+            mouse_x, mouse_y = event.pos
+            board_x, board_y = windowToBoardCoord(mouse_x, mouse_y)
+            grid[board_y][board_x].isObstacle = True
+            grid[board_y][board_x+1].isObstacle = True
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                if startset == True and endset == True:
+                    doPathFind = True
+            elif event.key == pygame.K_v:
+                visualizer = True
+    if doPathFind == True:
+        if end not in closed:
+            findAdjacent(currentnode.x, currentnode.y)
+            opendict = {}
+            for i in open:
+                opendict[i] = i.fCost
+            fmin = min(opendict.values()) 
+            minlist = [key for key in opendict if opendict[key] == fmin]
+            open.remove(grid[minlist[0].y][minlist[0].x])
+            closed.append(grid[minlist[0].y][minlist[0].x])
+            currentnode = grid[minlist[0].y][minlist[0].x]
+            bgupdategrid(visualizer)
+        else:
+            grid[currentnode.y][currentnode.x].getPath()
+            doPathFind = False
+    bgupdategrid(visualizer)
+    pygame.display.flip()
